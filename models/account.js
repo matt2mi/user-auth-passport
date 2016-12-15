81aa6a6922da2226b5dbba63d514aed049cba482
matt2mi <2m1tema@gmail.com>
2016-12-16 00:58:51 +0100

    debut ...

--- models/account.js
+++ models/account.js
@@ -12,3 +12,4 @@ Account.plugin(passportLocalMongoose);
 module.exports = mongoose.model('Account', Account);
 
 // from : http://mherman.org/blog/2015/01/31/local-authentication-with-passport-and-express-4/#.VZCK9xNViko
+// à Test Redux
