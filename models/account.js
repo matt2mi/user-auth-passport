09361b41837f45e358d5f497703303caff015d94
matt2mi <2m1tema@gmail.com>
2016-12-16 00:41:12 +0100

    first commit

---
+++ models/account.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var Account = new Schema({
+    username: String,
+    password: String
+});
+
+Account.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('Account', Account);
+
+// from : http://mherman.org/blog/2015/01/31/local-authentication-with-passport-and-express-4/#.VZCK9xNViko
